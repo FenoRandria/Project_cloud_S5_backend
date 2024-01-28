33d069e2297425e6d62929ac05545986ef67a610
FenoRandria <117942753+FenoRandria@users.noreply.github.com>
2024-01-28 21:57:53 +0300

    commit jointure terrain

---
+++ src/main/java/com/project_cloud_s5/hallo/service/Terrain_parcelle_culture_serve.java
@@ -0,0 +1,39 @@
+package com.project_cloud_s5.hallo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project_cloud_s5.hallo.dao.Terrain_dao;
+import com.project_cloud_s5.hallo.model.terrain.Terrain;
+
+@Service
+public class Terrain_parcelle_culture_serve {
+
+    private final Terrain_serve terrain;
+    private final Parcelle_serve parcelle;
+    private final Categorie_culture_serve culture;
+
+    @Autowired 
+    public Terrain_parcelle_culture_serve(Terrain_serve terrain,Parcelle_serve parcelle,Categorie_culture_serve culture)
+    {
+        this.terrain = terrain;
+        this.parcelle = parcelle;
+        this.culture = culture;
+    }   
+    public List getTerrain_parcelle_culture() 
+    {
+        List list = new ArrayList<>();
+        try {
+            list.add(terrain.getTerrains());
+            list.add(parcelle.getParcelles());
+            list.add(culture.getCategorie_cultures());
+        } catch (Exception e) {
+            throw e;
+        }
+        return list;
+    }
+    
+}
